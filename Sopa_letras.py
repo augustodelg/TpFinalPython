5857e56b3fc2f211b6a440f8f17e1de4f86bf16c
ger-velazquez <51427302+ger-velazquez@users.noreply.github.com>
2019-06-20 03:16:42 -0300

    #acatualizacion final

--- Sopa_letras.py
+++ Sopa_letras.py
@@ -14,9 +14,11 @@ LOGO="./IMAGENES/logosopa.png"
 JUGAR="./IMAGENES/jugar.png"
 CANCELAR="./IMAGENES/cancelar.png"
 DECORACION="./IMAGENES/logo_decoracion.png"
+REPORTE="./REPORTE/Reportes_aux.json"
 
     
 def mayor_palabra(palabras):
+	'''Retorna la longitud de la palabra mas grande de todas las palabras validadas '''
     max=-1
     for lis in palabras:
         for a in lis:
@@ -25,19 +27,20 @@ def mayor_palabra(palabras):
     return max
 
 def palabras_en_la_linea(coordenadas,x_o_y):
+	'''Retorna true o false, si hay una palabra en una determinada fila o una determinada columna '''
     for c in coordenadas:
         if c[0]== x_o_y:
             return True
     return False
 
 def interpretar_reporte(fuente,fuente2):
-    arch=open('Reportes_aux.json')
+	'''Analiza el archivo .json de reportes para mostrarlo en pantalla '''
+    arch=open("./REPORTE/Reportes_aux.json")
     data= json.load(arch)   
     tx='\n'
     for pal in data: 
         tx+= list(pal.keys())[0]+'  :'
         tx+= list(pal.values())[0]+' \n \n'
-    #sg.PopupScrolled(tx)
     if(fuente2=="MAYUSCULAS"):
         tx=tx.upper()
     else:
@@ -46,27 +49,36 @@ def interpretar_reporte(fuente,fuente2):
 
 #----------------------------CONFIGURACION---------------------------
 
-config =  [ [sg.T(" "),sg.Image(LOGO)],
-            [sg.T(" "  * 45),sg.Text(" OPCIONES DE PALABRAS ",font="Bahnschrift",background_color="#C6C6C6")],
-            [sg.T(" "  * 30),sg.InputText("",size=(40, 12),key="palabra_agregar"),sg.Button("Agregar",font="Bahnschrift 10",size=(10,1))],
+config1 =  [ 
+            [sg.T(" "),sg.Image(LOGO)],
+            [sg.T(" "  * 45),sg.Text(" OPCIONES DE PALABRAS ",font="Trebuchet",background_color="#C6C6C6")],
+            [sg.T(" "  * 30),sg.InputText("",size=(30, 12),key="palabra_agregar"),sg.Button("Agregar",font="Trebuchet 10",size=(10,1))],
             [sg.T(" "  * 30)],
-            [sg.T(" " ),sg.Listbox(values=[], size=(45, 5),key="lbox",font="Bahnschrift"),sg.Button("Eliminar",font="Bahnschrift 12",size=(10,5)),sg.T(" "  * 2)],
+            [sg.T(" " * 20 ),sg.Listbox(values=[], size=(25, 5),key="lbox",font="Trebuchet"),sg.Button("Eliminar",font="Trebuchet 12",size=(10,5)),sg.T(" "  * 2)],
             [sg.T(" "  * 30)],
-            [sg.T(" "  * 35),sg.Text("SUSTANTIVOS    -",font="Bahnschrift"),sg.Text("CANT:",font="Bahnschrift"), sg.InputCombo([0,1,2,3,4,5,6],key="cant_sustantivos",font="Bahnschrift 11"),sg.ColorChooserButton("COLOR",key="color_sustantivos")],
-            [sg.T(" "  * 35),sg.Text("VERBO    -",font="Bahnschrift"),sg.Text("CANT:",font="Bahnschrift"), sg.InputCombo([0,1,2,3,4,5,6],key="cant_verbos",font="Bahnschrift 11"),sg.ColorChooserButton("COLOR",key="color_verbos")],
-            [sg.T(" "  * 35),sg.Text("ADJETIVOS    -",font="Bahnschrift"),sg.Text("CANT:",font="Bahnschrift"), sg.InputCombo([0,1,2,3,4,5,6],key="cant_adjetivos",font="Bahnschrift 11"),sg.ColorChooserButton("COLOR",key="color_adjetivos")],
-            [sg.T(" "  * 30)],
-            [sg.T(" "  * 25),sg.Text("ORIENTACION DE LAS PALABRAS:",font="Bahnschrift"), sg.InputCombo(["HORIZONTAL","VERTICAL"],key="orientacion",font="Bahnschrift 10",size=(17,5))],
-            [sg.T(" "  * 25),sg.Text("¿QUE TIPO DE AYUDA DESEA DAR?:",font="Bahnschrift"), sg.InputCombo(["DEFINICIONES","LISTA DE PALABRAS"],key="ayuda",font="Bahnschrift 10",size=(17,5))],
-            [sg.T(" "  * 30)],
-            [sg.T(" "  * 50),sg.Text(" FORMA DE LETRAS ",font="Bahnschrift",background_color="#C6C6C6")], 
-            [sg.T(" "  * 30),sg.Text("FUENTE: ",font="Bahnschrift"),sg.InputCombo(["Bahnschrift","Trebuchet","Impact","Arial","Georgia",'ComicSansMS'],font="Bahnschrift 10",size=(12,5),key="fuente") ,sg.InputCombo(["MAYUSCULAS","MINUSCULAS"],font="Bahnschrift 10",size=(12,5),key= "may_o_min")],
-            [sg.T(" "  * 30)],
-            [sg.T(" "  * 55),sg.Button("VER REPORTE",font="Bahnschrift 10")],
-            [sg.T(" "  * 30)],
-            [sg.T(" "  * 2),sg.RButton("",button_color=sg.TRANSPARENT_BUTTON,image_filename=JUGAR, image_subsample=2, border_width=0,key="jugar"),sg.T(" "  * 2),sg.RButton("",image_filename=CANCELAR,button_color=sg.TRANSPARENT_BUTTON, image_subsample=2, border_width=0,key="Cancelar")],
+            [sg.T(" "  * 30),sg.RButton("",button_color=sg.TRANSPARENT_BUTTON,image_filename=JUGAR, image_subsample=2, border_width=0,key="jugar"),sg.T(" "  * 2),],
             [sg.T(" "  * 30)],
             ]
+            
+config2= [	[sg.T(" "  * 30)],[sg.T(" "  * 30)],[sg.T(" "  * 30)],
+            [sg.T(" "  * 35),sg.Text("SUSTANTIVOS    -",font="Trebuchet"),sg.Text("CANT:",font="Trebuchet"), sg.InputCombo([0,1,2,3,4,5,6],key="cant_sustantivos",font="Trebuchet 11"),sg.ColorChooserButton("COLOR",key="color_sustantivos")],
+            [sg.T(" "  * 35),sg.Text("VERBO    -",font="Trebuchet"),sg.Text("CANT:",font="Trebuchet"), sg.InputCombo([0,1,2,3,4,5,6],key="cant_verbos",font="Trebuchet 11"),sg.ColorChooserButton("COLOR",key="color_verbos")],
+            [sg.T(" "  * 35),sg.Text("ADJETIVOS    -",font="Trebuchet"),sg.Text("CANT:",font="Trebuchet"), sg.InputCombo([0,1,2,3,4,5,6],key="cant_adjetivos",font="Trebuchet 11"),sg.ColorChooserButton("COLOR",key="color_adjetivos")],
+            [sg.T(" "  * 30)],
+            [sg.T(" "  * 25),sg.Text("ORIENTACION DE LAS PALABRAS:",font="Trebuchet"), sg.InputCombo(["HORIZONTAL","VERTICAL"],key="orientacion",font="Trebuchet 10",size=(17,5))],
+            [sg.T(" "  * 25),sg.Text("¿QUE TIPO DE AYUDA DESEA DAR?:",font="Trebuchet"), sg.InputCombo(["DEFINICIONES","LISTA DE PALABRAS"],key="ayuda",font="Trebuchet 10",size=(17,5))],
+            [sg.T(" "  * 30)],
+            [sg.T(" "  * 50),sg.Text(" FORMA DE LETRAS ",font="Trebuchet",background_color="#C6C6C6")], 
+            [sg.T(" "  * 30),sg.Text("FUENTE: ",font="Trebuchet"),sg.InputCombo(["Trebuchet","Impact","Arial","Georgia",'ComicSansMS'],font="Trebuchet 10",size=(12,5),key="fuente") ,sg.InputCombo(["MAYUSCULAS","MINUSCULAS"],font="Trebuchet 10",size=(12,5),key= "may_o_min")],
+            [sg.T(" "  * 30)],
+            [sg.T(" "  * 55),sg.Button("VER REPORTE",font="Trebuchet 10")],
+            [sg.T(" "  * 30)],
+            [sg.T(" "  * 35),sg.RButton("",image_filename=CANCELAR,button_color=sg.TRANSPARENT_BUTTON, image_subsample=2, border_width=0,key="Cancelar")],
+            [sg.T(" "  * 30)],
+            ]
+config = [ 
+            [sg.Column(config1), sg.Column(config2)],		
+         ]
 
 palabras={"sustantivos":[],"verbos":[],"adjetivos":[]}
 
@@ -77,19 +89,16 @@ while True:
     try:
         eventC, configuracion = windowC.Read()
         if(eventC=="Agregar"):
+            sg.Popup('Procesando palabra... esto podria tardar unos segundos .Presione OK para continuar')
             palabra=configuracion["palabra_agregar"]
-            resul=Validador_palabra.validar_palabra(palabra)
-            print(resul)
-            input()
-            ''' if( es palabra valida):
-                    agregar al diccionario la palabra 
-                else:
-                    sg.PopError('La palabra no es valida')'''
-            lista_palabras.append(palabra)
-            aux=lbox.GetListValues()
-            if (not palabra in aux):#SI YA EXISTE LA PALABRA NO LA AGREGO
-                aux.append(palabra)
-                lbox.Update(aux)
+            resul=Validador_palabra.validar_palabra(palabra.lower())
+            if(resul[0]== True):
+                    sg.Popup("Palabra verificada correctamente!")
+                    palabras[list(resul[1].keys())[0]+"s"].append(list(resul[1].values())[0])
+                    aux=lbox.GetListValues()
+                    if (not palabra in aux):#SI YA EXISTE LA PALABRA NO LA AGREGO
+                        aux.append(palabra)
+                        lbox.Update(aux)
         if(eventC=="Eliminar"):
             a_eliminar=configuracion["lbox"][0]
             aux=lbox.GetListValues()
@@ -111,21 +120,15 @@ while True:
 # ------------------------ VAR --------------------------------------  
 
 cant_palabras={"Sustantivos":0,"Verbos":0 ,"Adjetivos":0}#DESPUES DE FILTRAR SE CUANTOS HAY REALMENTE
-for i in range(6):
-    palabras["sustantivos"].append(["hola"])
-print(palabras)
+
 
 tamaño_max=mayor_palabra(list(palabras.values()))
-print(tamaño_max)
 nume_palabras=0
 
 for lis in list(palabras.values()): nume_palabras+=len(lis)
 
-print(nume_palabras)
-input()
 cant_ubicaciones=int(tamaño_max)+nume_palabras#tiene que ser el canta de las palabras
 
-print(cant_ubicaciones)
 
 if(nume_palabras>12):
     BOX_SIZE=(BOX_SIZE * 25)//30
@@ -137,33 +140,34 @@ if(nume_palabras>12):
 
 sin_ayuda = [
             [sg.Image(DECORACION,size=(100,100))],
-			[sg.Text("Sustantivos:",text_color=configuracion["color_sustantivos"],font=configuracion["fuente"],background_color="#E6E6E6"),sg.Text(cant_palabras["Sustantivos"],font="Bahnschrift 10")],
-            [sg.Text("Verbos:",text_color=configuracion["color_verbos"],font=configuracion["fuente"],background_color="#E6E6E6"),sg.Text(cant_palabras["Verbos"],font="Bahnschrift 10")],
-			[sg.Text("Adjetivos:",text_color=configuracion["color_adjetivos"],font=configuracion["fuente"],background_color="#E6E6E6"),sg.Text(cant_palabras["Adjetivos"],font="Bahnschrift 10")],
-			
-			]
+            [sg.Text("Sustantivos:",text_color=configuracion["color_sustantivos"],font=configuracion["fuente"],background_color="#E6E6E6"),sg.Text(len(palabras["sustantivos"]),font="Trebuchet 10")],
+            [sg.Text("Verbos:",text_color=configuracion["color_verbos"],font=configuracion["fuente"],background_color="#E6E6E6"),sg.Text(len(palabras["verbos"]),font="Trebuchet 10")],
+            [sg.Text("Adjetivos:",text_color=configuracion["color_adjetivos"],font=configuracion["fuente"],background_color="#E6E6E6"),sg.Text(len(palabras["adjetivos"]),font="Trebuchet 10")],
+            
+            ]
 
 
 sopa = [
         [sg.Graph(canvas_size=(cant_ubicaciones*38, cant_ubicaciones*38), graph_bottom_left=(0,cant_ubicaciones*26), graph_top_right=(cant_ubicaciones*26,0), change_submits=True, drag_submits=False,background_color="white", key="graph", tooltip="SELECCIONE LAS LETRAS QUE CONFORMAN LAS PALABRAS!!")],
         [sg.T(" "  * 30)],
-        [sg.Button("Controlar",font="Bahnschrift 10",size=(20,3))],
+        [sg.Button("Controlar",font="Trebuchet 10",size=(20,3))],
         ]
 
 if (sg.PopupYesNo("QUIERE LAS AYUDAS?")=="Yes"):
-    sopa.append([sg.Text("INFORMACION DE AYUDA:",font="Bahnschrift 10")],)
+    sin_ayuda.append([sg.Text("INFORMACION DE AYUDA:",font="Trebuchet 10")],)
     texto=""
     for t in palabras.keys():
             for info in palabras[t]:
-                if(configuracion["ayuda"] == "DEFINICIONES"):
-                    texto+= info[1]+" \n"
-                else:
-                    texto+=info[0]+" \n"
-    sopa.append([sg.Multiline(texto, size=(80,10),key="info_ayuda",font=configuracion["fuente"])],)   
-    sopa.append([sg.T(" "  * 30)])    
+                if(len(info) !=0):
+                    if(configuracion["ayuda"] == "DEFINICIONES"):
+                        texto+= info[1]+" \n \n"
+                    else:
+                        texto+=info[0]+" \n \n"
+    sin_ayuda.append([sg.Multiline(texto, size=(30,10),key="info_ayuda",font=configuracion["fuente"],enter_submits=True,disabled=True)],)   
+    sin_ayuda.append([sg.T(" "  * 30)])    
 
 layout = [ 
-			[sg.Column(sopa), sg.Column(sin_ayuda,)],		
+            [sg.Column(sopa), sg.Column(sin_ayuda,)],		
          ]   
 
 
@@ -174,7 +178,6 @@ window = sg.Window("SOPA DE LETRAS", layout, grab_anywhere=True).Finalize()
 
 
 g = window.Element("graph")
-#graph.DrawRectangle(top_left=[tamaño_max*-17,tamaño_max*30], bottom_right=[10,50], fill_color="white", line_color="black")
 
 #-----------------------------------------------------------------------
 
@@ -190,14 +193,7 @@ for i in range(cant_ubicaciones):
 #-------------------------MATRIZ DIBUJO-----------------------------------------------
 
 for row in range(cant_ubicaciones):
-    print("VALOR DEL ROW " +str(row))
     for col in range(cant_ubicaciones):
-            print("#############")	
-            print("columna "+str(col),"ROW "+str(row))
-            print(col * BOX_SIZE + 5,row * BOX_SIZE + 3)
-            print("-------")
-            print(col * BOX_SIZE + BOX_SIZE + 5,row * BOX_SIZE + BOX_SIZE + 3)
-            print("#############")
             matriz[col][row]=g.DrawRectangle((col * BOX_SIZE + 5, row * BOX_SIZE + 3), (col * BOX_SIZE + BOX_SIZE + 5, row * BOX_SIZE + BOX_SIZE + 3), line_color="black")
 
 #---------------------------------------------------------------------------------------
@@ -220,19 +216,12 @@ for tipo in palabras.keys():
         X=random.randint(0,cant_ubicaciones-2)
         Y=random.randint(0,cant_ubicaciones-2)
         while not insertada:
-            print("LA RAW: "+str(X)+" LA COL: "+str (Y))
             if(configuracion["orientacion"]== "HORIZONTAL"):
-                print('CANTIDA DE ESPACIOS'+str(cant_ubicaciones - Y))
                 while(cant_ubicaciones - Y < len(palabra[0])):#PARA QUE LA PALABRA NO SE IMPRIMA FUERA DEL RANGO DE LA MATRIZ 
-                    print(""+str(cant_ubicaciones-Y))
-                    print(Y- cant_ubicaciones < len(palabra[0]))
                     Y-=1
             else:
                 while(cant_ubicaciones - X < len(palabra[0])):#PARA QUE LA PALABRA NO SE IMPRIMA FUERA DEL RANGO DE LA MATRIZ 
-                    print(""+str(cant_ubicaciones-X))
-                    print(X- cant_ubicaciones < len(palabra[0]))
                     X-=1
-                    #input()
             if(configuracion["orientacion"]== "HORIZONTAL"):
                             coor=(X,range(Y,Y+len(palabra[0])))
             else:
@@ -240,11 +229,9 @@ for tipo in palabras.keys():
                             
             if(configuracion["orientacion"]== "HORIZONTAL"):
                             hay_palabra=palabras_en_la_linea(lista_coor_palabras,X)
-                            print( palabras_en_la_linea(lista_coor_palabras,X))
 
             else:
                             hay_palabra=palabras_en_la_linea(lista_coor_palabras,Y)
-                            print("HAY LINEA DIO:"+str(palabras_en_la_linea(lista_coor_palabras,Y)))
             if(not hay_palabra):
                     yI=Y
                     xI=X
@@ -253,7 +240,6 @@ for tipo in palabras.keys():
                             letra=letra.upper()
                         else:
                             letra=letra.lower()
-                        print("LUGAR DE IMPRESION = COL" +str(yI)+ "  ROW "+str(xI) )
                         g.DrawText(letra , (yI * BOX_SIZE + 18, xI * BOX_SIZE + 17),font=Fuente)
                         matriz3[yI][xI]=configuracion["color_"+ tipo]
                         if(configuracion["orientacion"]== "HORIZONTAL"):
@@ -263,7 +249,6 @@ for tipo in palabras.keys():
                     lista_coor_palabras.append(coor)
                     insertada=True
             else:
-                    print("ENTRE AL ELSE")
                     X=random.randint(0,cant_ubicaciones)
                     Y=random.randint(0,cant_ubicaciones)
 
@@ -282,17 +267,12 @@ for row in range(cant_ubicaciones):
             if(matriz3[row][col] == "white"):
                     g.DrawText(random.choice(letra) , (row * BOX_SIZE + 18, col * BOX_SIZE + 17),font=Fuente)
 
-#-------------------------ECRIBIR PALABRA-----------------------------------------------
+#-------------------------ESCRIBIR PALABRA-----------------------------------------------
 
-#g.DrawText(random.choice(string.ascii_uppercase) , (col * BOX_SIZE + 18, row * BOX_SIZE + 17),font='Bahnschrift 20')
-
-print(matriz)
-print(matriz2)
 
 while True:             # Event Loop
     try:
         event, values = window.Read()
-        print(event, values)
         if event is None or event == "Exit":
             break
         mouse = values["graph"]
@@ -302,37 +282,25 @@ while True:             # Event Loop
                 continue
             box_x = mouse[0]//BOX_SIZE
             box_y = mouse[1]//BOX_SIZE
-            '''	print(box_x, box_y)
-            print(letter_location)
-            print('----------')
-            print(box_x)
-            print(box_y)'''
             if(matriz2[box_x][box_y] == False):
                 if (matriz3[box_x][box_y]!="white"):
                     g.TKCanvas.itemconfig(matriz[box_x][box_y], fill=matriz3[box_x][box_y])
                 else:
                     g.TKCanvas.itemconfig(matriz[box_x][box_y], fill="#CFF5E3")
                 matriz2[box_x][box_y] = True
-                print(matriz2[box_x][box_y])
             else :
                 g.TKCanvas.itemconfig(matriz[box_x][box_y], fill="white")
                 matriz2[box_x][box_y] = False
-                #print(g.TKFrame(matriz[box_x][box_y], fill_color))
         elif (event=="Controlar"):
              sg.Popup("TUS PALABRAS/LETRAS FALTANTES VAN A SER MARCADAS EN GRIS!")
-             print(lista_coor_palabras)
              for cor in lista_coor_palabras:
                 y=cor[0]
                 for x in cor[1]:
                     if  not(matriz2[x][y]):
-                        print("NO ESTA MARCADO")
                         if(configuracion["orientacion"]== "HORIZONTAL"):
                             g.TKCanvas.itemconfig(matriz[x][y], fill="#C5C6C5")
                         else:
                             g.TKCanvas.itemconfig(matriz[y][x], fill="#C5C6C5")
-                    else:
-                        print("Esta marcado")
-                
             #g.DrawRectangle((box_x * BOX_SIZE + 5, box_y * BOX_SIZE + 3),(box_x * BOX_SIZE + BOX_SIZE + 5, box_y * BOX_SIZE + BOX_SIZE + 3) ,fill_color='#CFF5E3', line_color='black')
     except(IndexError):
         continue
--- Validador_palabra.py
+++ Validador_palabra.py
@@ -8,7 +8,11 @@ from pattern.es import parse,split
 
 
 def validar_palabra(word):
-    lis_resultados = [None,None] #lista a retornar, donde la posicion 0 dice si la palabra es válida, y en la posicion 1, un diccionario.
+    '''validar_palabra devolvera una lista con dos elementos. 
+        En el primero un booleano indicando si la palabra es válida (o no), 
+        y el segundo es un diccionario donde la clave es la palabra, y el
+        valor es la definicion   '''
+    lis_resultados = [None,None] #lista a retornar.
     lis_tipo_validos = ["sustantivo","adjetivo","verbo"]
     wik = Wiktionary(language="es")
     valida = False
@@ -28,7 +32,11 @@ def validar_palabra(word):
                 if (tipo != "zzz"): #esto sucede cuando la palabra encontrada es de alguno de los tipos tratatos en evaluar()
                     if tipo in lis_tipo_validos:
                         definicion = buscar_definicion(word,section.string)
-                        valida = True
+                        if definicion!= "error":
+                            valida = True
+                        else:
+                            valida=False
+                            definicion=None
                         if (tipo!=tipo_pattern(word)):
                             generar_reporte(word,"La palabra es valida, sin embargo algunos criterios de busqueda pudieron diferir")
                     else:
@@ -39,8 +47,8 @@ def validar_palabra(word):
             return lis_resultados
        
         except AttributeError: # Error en el caso de que la palabra no exista en wiktionary
-            tipo_aux = tipo_pattern(word)
-            lis_consul =consulta(word,tipo_aux)########################################3
+            tipo_aux = tipo_pattern(word) 
+            lis_consul =consulta(word,tipo_aux)#consulta devolvera una lista, donde la primer posicion indica la validez de la palabra, y la segunda una definicion. Aqui hay bastante interaccion con el usuario
             if (lis_consul[0]==False):
                 generar_reporte(word,"La palabra no es valida, ya que ningun criterio de busqueda pudo determinarla")
             lis_resultados[0] = lis_consul[0]
@@ -48,44 +56,57 @@ def validar_palabra(word):
             return lis_resultados
 
 def buscar_definicion(word,texto):
-    punto_final=0
-    punto_index=0
-    cont=0
-    definicion=""
-    for caracter in texto:
-        if caracter.isdigit():
-            numero_index=cont
-            break
-        cont=cont+1	
+    '''Procesa texto(section.string), filtrando y devolviendo una definicion 
+        para la palabra'''
+    try:
+        punto_final=0
+        punto_index=0
+        cont=0
+        definicion=""
+        for caracter in texto:
+            if caracter.isdigit():
+                numero_index=cont
+                break
+            cont=cont+1	
 
-    for x in range(numero_index,len(texto)+1):
-        if texto[x] ==".":
-            punto_index=cont
-            break
-        cont=cont+1
-
-    aux=texto[punto_index+1]
-
-    if aux.isalpha():
-        for y in range (punto_index+1,len(texto)+1):
-            if texto[y] ==".":
-                punto_final=cont
+        for x in range(numero_index,len(texto)+1):
+            if texto[x] ==".":
+                punto_index=cont
                 break
             cont=cont+1
-        definicion = texto[numero_index+1:punto_final+1]
-    else:
-        definicion =texto[numero_index+1:punto_index+1]
+            
+        if punto_index+1<len(texto):
+            aux=texto[punto_index+1]
+            if aux.isalpha():
+                for y in range (punto_index+1,len(texto)+1):
+                    if texto[y] ==".":
+                        punto_final=cont
+                        break
+                    cont=cont+1
+                definicion = texto[numero_index+1:punto_final+1]
+            else:
+                definicion =texto[numero_index+1:punto_index+1]
+        else:
+            definicion =texto[numero_index+1:punto_index]
+                
+        return definicion
+    
+    except IndexError: #Excepcion en el caso de que haya ocurrido algun problema en la obtencion de la definicion, producto de un mal formato en Wiktionary
+        sg.PopupError('No es posible cargar esa palabra. Intente con una nueva')
+        return "error"
         
-    return definicion
+        
 
 
 def generar_reporte(palabra,motivo):
-     if os.path.isfile('Reportes_aux.json')==False or (os.path.getsize('Reportes_aux.json')==0):
-         with open("Reportes_aux.json", "w") as jsonFile:
+    ''' Agrega palabras a un reporte; en caso de que los criterios de busqueda
+        no coincidan o que no se haya encontrado la palabra bajo ningun criterio, inclusive por la validacion del usuario'''
+     if os.path.isfile('./REPORTE/Reportes_aux.json')==False or (os.path.getsize('./REPORTE/Reportes_aux.json')==0):
+         with open("./REPORTE/Reportes_aux.json", "w") as jsonFile:
              datos = [{palabra:motivo}]
              json.dump(datos, jsonFile,indent=4)
      else:
-         with open("Reportes_aux.json", "r+") as jsonFile:
+         with open("./REPORTE/Reportes_aux.json", "r+") as jsonFile:
              data = json.load(jsonFile)
              data.append({palabra:motivo})
              jsonFile.seek(0)
@@ -94,7 +115,9 @@ def generar_reporte(palabra,motivo):
 
                            
 
-def consulta(word,tipo): #interfaz para verificar si el usuario desea quedarse con la palabra, aunque no este en wikt.
+def consulta(word,tipo):
+    '''Interfaz para verificar si el usuario desea quedarse con la palabra, aunque no este en wikt, pero con una clasificacion de pattern.es. 
+        Si asi lo desea, deberá ingresar una definicion manualmente '''
     layout = [[sg.Text('La palabra ' +word+ ' aparentemente es un '+tipo+'.¿Desea conservar palabra?')],
               [sg.Button('Si'), sg.Button('No')]]
 
@@ -104,13 +127,13 @@ def consulta(word,tipo): #interfaz para verificar si el usuario desea quedarse c
     while True:
         ok = True
         event, values = window.Read()
-        if event is None or event == 'Si': #La persona desea conservar la palabra. Pido una definición por teclado
+        if event == 'Si': #La persona desea conservar la palabra. Pido una definición por teclado
           definicion = escribir_definicion(word)
           if (definicion == "False"):
               continue
           lis_resul[1] = definicion
           break
-        if event == 'No':
+        if event is None or event == 'No':
             ok=False #La persona no desea conservar la palabra
             break
     window.Close()
@@ -141,7 +164,8 @@ def escribir_definicion(word): #interfaz para ingresar la definicion de una pala
     
     return definicion
 
-def tipo_pattern(word): #retorna el tipo gramatico de la palabra, definido por pattern.es
+def tipo_pattern(word):
+    '''Retorna el tipo gramatico de la palabra, definido por pattern.es '''
     diccionario_de_tipos = {"NN": "sustantivo", "JJ": "adjetivo", "VB": "verbo"}
     aux = parse(word).split("/")
     tipo_pat = aux[1] #NN.JJ.VB u otros
@@ -156,6 +180,7 @@ def tipo_pattern(word): #retorna el tipo gramatico de la palabra, definido por p
      
 
 def evaluar(title_aux,tipo):
+    '''Retorna el tipo gramatico de la palabra, definido por Wiktionary '''
     lis_gramatica = ["Artículo", "Pronombre", "Adverbio","Interjección", "Preposición", "Conjunción"]
     if (title_aux[0] == 'Verbo'):
         tipo = "verbo"
